57df1127da6a06f83489e9c1e2dc288f6a4a220b
1026903031 <1026903031@qq.com>
2020-04-24 15:56:16 +0800

    添加了三个隐藏栏和一些优化

--- 二轮考核/land.js
+++ 二轮考核/land.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    //免密码登录和密码登录切换
+    //免密码登录和密码登录和二维码切换  因为获取类元素的时候总是会报错，就麻烦了点
     var right = document.getElementById("right");
     var left = document.getElementById("left");
     var rightOpen = document.getElementById("rightOpen");
@@ -50,50 +50,121 @@ window.onload = function () {
     }
 
 
-    var bColor = document.querySelector(".land");
-
+    
+    /*背景随时间变化而变化*/
+    var land = document.querySelector(".land");
+    var homepage = document.querySelector(".homepage");
     var date = new Date();
     var hour = date.getHours();
 
     if (hour >= 0 && hour <6){
-        bColor.style.backgroundColor = "rgb(0,80,114)";
+        land.style.backgroundColor = "rgb(0,80,114)";
         console.log("凌晨，早点睡");
     }
     if (hour >= 6 && hour <9){
-        bColor.style.backgroundColor = "rgb(86,205,254)";
+        land.style.backgroundColor = "rgb(86,205,254)";
         console.log("早睡早起身体好");
     }
     if (hour >= 9 && hour <17){
-        bColor.style.backgroundColor = "rgb(184，229，248)";
+        land.style.backgroundColor = "rgb(184，229，248)";
         console.log("好好学习");
     }
     if (hour >= 17 && hour <19){
-        bColor.style.backgroundColor = "rgb(86,205,254)";
+        land.style.backgroundColor = "rgb(86,205,254)";
         console.log("吃顿好的晚餐，以战晚上的代码");
     }
     if (hour >= 19 && hour < 24){
-        bColor.style.backgroundColor = "rgb(0,80,114)";
+        land.style.backgroundColor = "rgb(0,80,114)";
         console.log("打打打打代码");
     }
 
+    /*登录跳转*/
+
+    var goto_homepage = document.getElementById("goto_homepage");
+
+    goto_homepage.onclick = function() {
+        land.style.display = "none";
+        homepage.style.display = "block";
+
+    }
+
+
+    /*主页导航条的消失与隐藏*/
+    var bell = document.getElementById("bell");
+    var bell_show = document.querySelector(".bell_show");
+    var information = document.getElementById("information");
+    var information_show = document.querySelector(".information_show");
+    var people = document.getElementById("people");
+    var people_show = document.querySelector(".people_content");
+
+    people.onclick = function() {
+        if (people_show.style.display == "none") {
+            people_show.style.display = "block";
+            information_show.style.display = "none";
+            bell_show.style.display = "none";
+        } else {
+            people_show.style.display = "none";
+        }
+    }
+
+    bell.onclick = function() {
+        if (bell_show.style.display == "none") {
+            bell_show.style.display = "block";
+            information_show.style.display = "none";
+            people_show.style.display = "none";
+        } else {
+            bell_show.style.display = "none";
+        }
+    }
+
+    information.onclick = function() {
+        if (information_show.style.display == "none") {
+            information_show.style.display = "block";
+            bell_show.style.display = "none";
+            people_show.style.display = "none";
+        } else {
+            information_show.style.display = "none";
+        }
+
+    }
+
+    var homepage_main = document.querySelector(".homepage_main");
+
+    homepage_main.onclick = function() {
+        bell_show.style.display = "none";
+        people_show.style.display = "none";
+        information_show.style.display = "none";
+    }
+
+    /*返回滑动条数值*/
+    function scroll() {
+        return {
+            "top": document.documentElement.scrollTop + document.body.scrollTop,        //返回滑动条高度
+            "left": document.documentElement.scrollLeft + document.body.scrollLeft      //返回滑动条左侧距离
+        }
+    }
 
     
-    //顶部导航条左右移动
+    /*/顶部导航条左右移动
     var header_roll = document.getElementById("header_roll");
     var roll = scroll().left;
     
     window.onscroll = function () {
-
-        if (scroll().left > 100) {
+        if (scroll().left > -50) {
             console.log(123);
-            header_roll.style.left = roll +'px' ;
-            
-            console.log(100-scroll().left);
         } 
-    }
+
+        if (scroll().left > 1) {
+            header_roll.style.left = roll +"-50px" ;
+        } 
+    }*/
 
     //回到顶部
     var goTop = document.getElementById("goTop");
+
+    /*侧边条条框在一定程度下浮动*/
+    var tofixedone = document.getElementById("tofixedone");
+    var tofixedtwo = document.getElementById("tofixedtwo");
 
     window.onscroll = function () {
         if (scroll().top > 200) {
@@ -103,6 +174,20 @@ window.onload = function () {
             goTop.style.display = "none" ;
             header_roll.style.animationName = "topdown";
         }
+
+        if (scroll().top > 400) {
+            tofixedone.style.position = "fixed";
+            tofixedtwo.style.position = "fixed";
+            tofixedone.style.top = "60px";
+            tofixedtwo.style.top = "360px";
+
+            tofixedone.style.width = "290px";
+ 
+         } else {
+             tofixedtwo.style.position = "static";
+             tofixedone.style.position = "static";
+             tofixedone.style.width = "100%";
+         }
     }
     
     goTop.onclick = function () {                                           //当所设置gotop元素被点击时，触发函数
@@ -117,15 +202,7 @@ window.onload = function () {
         }, 30)
     }
 
-    function scroll() {
-        return {
-            "top": document.documentElement.scrollTop + document.body.scrollTop,        //返回滑动条高度
-            "left": document.documentElement.scrollLeft + document.body.scrollLeft      //返回滑动条左侧距离
-        }
-    }
-
-
-
+    
 
 
 }
